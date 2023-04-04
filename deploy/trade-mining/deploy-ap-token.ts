fbf9abbdc93f75711b427c168be6fe925279ac79
abcdefu-alpaca <abcdefu.alpaca@gmail.com>
2023-04-04 17:06:01 +0700

    fix: remove ap form token list

--- deploy/trade-mining/deploy-ap-token.ts
+++ deploy/trade-mining/deploy-ap-token.ts
@@ -1,33 +1,35 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DeployFunction } from "hardhat-deploy/types"
-import { ethers, upgrades, tenderly, network } from "hardhat"
-import { getConfig, writeConfigFile } from "../utils/config"
-import { getImplementationAddress } from "@openzeppelin/upgrades-core"
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, upgrades, tenderly, network } from "hardhat";
+import { getConfig, writeConfigFile } from "../utils/config";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const config = getConfig()
+  const config = getConfig();
 
-  const deployer = (await ethers.getSigners())[0]
-  const AP = await ethers.getContractFactory("AP", deployer)
-  const alp = await upgrades.deployProxy(AP)
-  await alp.deployed()
-  console.log(`Deploying AP Token Contract`)
-  console.log(`Deployed at: ${alp.address}`)
+  const deployer = (await ethers.getSigners())[0];
+  const AP = await ethers.getContractFactory("AP", deployer);
+  const ap = await upgrades.deployProxy(AP);
 
-  config.Tokens.AP = alp.address
-  config.TradeMining.miningPoint = alp.address
-  writeConfigFile(config)
+  console.log(`> ⛓ Tx submitted: ${ap.deployTransaction.hash}`);
+  console.log(`> Waiting tx to be mined...`);
+  await ap.deployed();
+  console.log(`Deploying AP Token Contract`);
+  console.log(`Deployed at: ${ap.address}`);
+
+  config.TradeMining.miningPoint = ap.address;
+  writeConfigFile(config);
 
   const implAddress = await getImplementationAddress(
     network.provider,
-    alp.address
-  )
+    ap.address
+  );
 
   await tenderly.verify({
     address: implAddress,
     name: "AP",
-  })
-}
+  });
+};
 
-export default func
-func.tags = ["APToken"]
+export default func;
+func.tags = ["APToken"];
